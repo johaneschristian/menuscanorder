e095f500fa591ab0bcd3d70d6fdc952416f61277
johanes.christian <johanes.christian@ui.ac.id>
2024-05-12 19:06:39 +1000

    Fix indentation of login page

--- app/Views/login-page.php
+++ app/Views/login-page.php
@@ -5,23 +5,23 @@
 
 <?= $this->section('content') ?>
 <section class="d-flex flex-row justify-content-center p-5">
-	<form action="" method="post" class="card shadow-lg rounded p-3 w-mdc-25 w-100">
-		<input type="hidden" name="<?= csrf_token() ?>" value="<?= csrf_hash() ?>">
-		<div class="text-center">
-			<span class="fw-bold fs-4 mb-3">Login</span>
-		</div>
-		<div class="mb-3">
-			<label for="" class="form-label fw-bold lh-sm">Email</label>
-			<input name="email" type="email" class="form-control bg-soft-gray">
-		</div>
-		<div class="mb-3">
-			<label for="" class="form-label fw-bold lh-sm">Password</label>
-			<input name="password" type="password" class="form-control bg-soft-gray">
-		</div>
-		<div class="d-flex flex-row justify-content-between align-items-center mt-3">
-			<a href="<?= base_url('register') ?>">Does not have an account?</a>
-			<button class="btn bg-brown text-light">Login</button>
-		</div>
-	</form>
+  <form action="" method="post" class="card shadow-lg rounded p-3 w-mdc-25 w-100">
+    <input type="hidden" name="<?= csrf_token() ?>" value="<?= csrf_hash() ?>">
+    <div class="text-center">
+      <span class="fw-bold fs-4 mb-3">Login</span>
+    </div>
+    <div class="mb-3">
+      <label for="" class="form-label fw-bold lh-sm">Email</label>
+      <input name="email" type="email" class="form-control bg-soft-gray">
+    </div>
+    <div class="mb-3">
+      <label for="" class="form-label fw-bold lh-sm">Password</label>
+      <input name="password" type="password" class="form-control bg-soft-gray">
+    </div>
+    <div class="d-flex flex-row justify-content-between align-items-center mt-3">
+      <a href="<?= base_url('register') ?>">Does not have an account?</a>
+      <button class="btn bg-brown text-light">Login</button>
+    </div>
+  </form>
 </section>
 <?= $this->endSection() ?>
